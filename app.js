f1ea33633e91832fc8e477fb3b619dc092d8cd54
Daniel Perevoshchikov <97989344+peregrinusss@users.noreply.github.com>
2022-06-17 16:20:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,38 +22,3 @@ cardBtns.forEach((cardBtn) => {
         chooseCard(card)
     })
 })
-
-/*
-// Hover choosed
-cards.forEach((card) => {
-    const parentCard = card.parentNode
-    if (parentCard.classList.contains('choosed')) {
-        var flag = 0
-        card.addEventListener('mouseover', () => {
-            if (flag === 0) {
-                addItem(card)
-                flag = 1
-            }
-        })
-        card.addEventListener('mouseleave', () => removeItem(card))
-    }
-})
-
-function addItem(card) {
-    const cardText = card.childNodes[1]
-    const cardTextFirstChild = cardText.childNodes[1]
-    const cardHoverText = document.createElement('div')
-    cardHoverText.innerHTML = "Котэ не одобряет?"
-    cardHoverText.style.color = '#E62E7A'
-    cardHoverText.style.fontSize = '16px'
-
-
-    cardText.insertBefore(cardHoverText, cardTextFirstChild)
-}
-
-function removeItem(card) {
-    const cardHoverText = card.childNodes[1].childNodes[1]
-    console.log(cardHoverText)
-    cardHoverText.remove()
-}
-*/
